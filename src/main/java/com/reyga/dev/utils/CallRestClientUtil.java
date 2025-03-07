d28c3225ec19be9f1b149a0a1c90b3dee355660f
ReygaFitra <Reyga.Fitra.Ramadhan@bni.co.id>
2025-03-07 09:08:50 +0700

    Add RestClient Component with Headers

--- src/main/java/com/reyga/dev/utils/CallRestClientUtil.java
+++ src/main/java/com/reyga/dev/utils/CallRestClientUtil.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.Map;
 import java.util.function.Predicate;
 
 @Component
@@ -205,6 +206,167 @@ public class CallRestClientUtil {
                 .retrieve()
                 .onStatus(statusError, errorHandler)
                 .toBodilessEntity();
+    }
+
+    /**
+     * Using RestClient Headers.
+     */
+
+    public <RES> RES get(String uri, MediaType mediaType, Class<RES> responseType, Map<String, String> headers) {
+        return restClient.get()
+                .uri(uri)
+                .accept(mediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .retrieve()
+                .body(responseType);
+    }
+
+    public <RES> RES post(String uri, MediaType contentType, MediaType acceptMediaType, Object requestType, Class<RES> responseType, Map<String, String> headers) {
+        return restClient.post()
+                .uri(uri)
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .body(requestType)
+                .retrieve()
+                .body(responseType);
+    }
+
+    public <RES> RES put(String uri, MediaType contentType, MediaType acceptMediaType, Object requestType, Class<RES> responseType, Map<String, String> headers) {
+        return restClient.put()
+                .uri(uri)
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .body(requestType)
+                .retrieve()
+                .body(responseType);
+    }
+
+    public <RES> RES delete(String uri, MediaType mediaType, Class<RES> responseType, Map<String, String> headers) {
+        return restClient.delete()
+                .uri(uri)
+                .accept(mediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .retrieve()
+                .body(responseType);
+    }
+
+    public ResponseEntity<Void> getWithBodilessEntity(String uri, MediaType mediaType, Map<String, String> headers) {
+        return restClient.get()
+                .uri(uri)
+                .accept(mediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public ResponseEntity<Void> postWithBodilessEntity(String uri, MediaType contentType, MediaType acceptMediaType, Object requestType, Map<String, String> headers) {
+        return restClient.post()
+                .uri(uri)
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .body(requestType)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public ResponseEntity<Void> putWithBodilessEntity(String uri, MediaType contentType, MediaType acceptMediaType, Object requestType, Map<String, String> headers) {
+        return restClient.put()
+                .uri(uri)
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .body(requestType)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public ResponseEntity<Void> deleteWithBodilessEntity(String uri, MediaType mediaType, Map<String, String> headers) {
+        return restClient.delete()
+                .uri(uri)
+                .accept(mediaType)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    public <RES> RES getWithErrorHandling(String uri, Map<String, String> headers, MediaType mediaType, Class<RES> responseType, Predicate<HttpStatusCode> statusError, RestClient.ResponseSpec.ErrorHandler errorHandler) {
+        return restClient.get()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .accept(mediaType)
+                .retrieve()
+                .onStatus(statusError, errorHandler)
+                .body(responseType);
+    }
+
+    public <RES> RES getWithErrorHandling(String uri, Map<String, String> headers, MediaType mediaType, Class<RES> responseType, RestClient.RequestHeadersSpec.ExchangeFunction<RES> fullResponseHandler) {
+        return restClient.get()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .accept(mediaType)
+                .exchange(fullResponseHandler);
+    }
+    public <RES> RES postWithErrorHandling(String uri, Map<String, String> headers, MediaType contentType, MediaType acceptMediaType, Object requestType, Class<RES> responseType,
+                                           Predicate<HttpStatusCode> statusError, RestClient.ResponseSpec.ErrorHandler errorHandler) {
+        return restClient.post()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .body(requestType)
+                .retrieve()
+                .onStatus(statusError, errorHandler)
+                .body(responseType);
+    }
+
+    public <RES> RES postWithErrorHandling(String uri, Map<String, String> headers, MediaType contentType, MediaType acceptMediaType, RestClient.RequestHeadersSpec.ExchangeFunction<RES> fullResponseHandler) {
+        return restClient.post()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .exchange(fullResponseHandler);
+    }
+    public <RES> RES putWithErrorHandling(String uri, Map<String, String> headers, MediaType contentType, MediaType acceptMediaType, Object requestType, Class<RES> responseType,
+                                          Predicate<HttpStatusCode> statusError, RestClient.ResponseSpec.ErrorHandler errorHandler) {
+        return restClient.put()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .body(requestType)
+                .retrieve()
+                .onStatus(statusError, errorHandler)
+                .body(responseType);
+    }
+
+    public <RES> RES putWithErrorHandling(String uri, Map<String, String> headers, MediaType contentType, MediaType acceptMediaType, RestClient.RequestHeadersSpec.ExchangeFunction<RES> fullResponseHandler) {
+        return restClient.put()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .contentType(contentType)
+                .accept(acceptMediaType)
+                .exchange(fullResponseHandler);
+    }
+    public <RES> RES deleteWithErrorHandling(String uri, Map<String, String> headers, MediaType mediaType, Class<RES> responseType, Predicate<HttpStatusCode> statusError, RestClient.ResponseSpec.ErrorHandler errorHandler) {
+        return restClient.delete()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .accept(mediaType)
+                .retrieve()
+                .onStatus(statusError, errorHandler)
+                .body(responseType);
+    }
+
+    public <RES> RES deleteWithErrorHandling(String uri, Map<String, String> headers, MediaType mediaType, Class<RES> responseType, RestClient.RequestHeadersSpec.ExchangeFunction<RES> fullResponseHandler) {
+        return restClient.get()
+                .uri(uri)
+                .headers(httpHeaders -> headers.forEach(httpHeaders::set))
+                .accept(mediaType)
+                .exchange(fullResponseHandler);
     }
 
 }
